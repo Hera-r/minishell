47657bd9c4427ab84c8cb616773e844ca779313b
Hera-r <hrandria@student.42.fr>
2024-03-14 19:42:11 +0100

    feat: implement parser and node checks

---
+++ src/check_node.c
@@ -0,0 +1,97 @@
+#include "../header/minishell.h"
+
+static void	error_return(char *cmd, t_data *data)
+{
+	if (ft_strcmp(cmd, "|") == SUCCESS)
+		(*data).ret = err_msg(SYNTAX_ERROR, cmd, 2);
+	else
+		(*data).ret = err_msg(SYNTAX_ERROR, "newline", 2);
+}
+
+static int	check_close_quote(char *cmd)
+{
+	if (check_unclosed_single(cmd) == FAIL)
+		return (FAIL);
+	if (check_unclosed_double(cmd) == FAIL)
+		return (FAIL);
+	return (SUCCESS);
+}
+
+int	check_error_node(t_node **node_tab, int size, t_data *data)
+{
+	int	i;
+
+	i = 0;
+	while (i < size)
+	{
+		if ((*node_tab)[i].type == T_ERROR && \
+			check_close_quote((*node_tab)[i].token) == FAIL)
+		{
+			(*data).ret = err_msg(UNCLOSED_ERROR, (*node_tab)[i].token, 1);
+			return (FAIL);
+		}
+		else if ((*node_tab)[i].type == T_ERROR && (i + 1) == size && \
+				ft_strlen((*node_tab)[i].token) <= 2)
+			return (error_return((*node_tab)[i].token, data), \
+					clean_node_error(node_tab, size), FAIL);
+		else if ((*node_tab)[i].type == T_ERROR)
+		{
+			(*data).ret = err_msg(SYNTAX_ERROR, (*node_tab)[i].token, 2);
+			return (clean_node_error(node_tab, size), FAIL);
+		}
+		i++;
+	}
+	return (SUCCESS);
+}
+
+void	check_redir_node(t_node **node_tab, int size)
+{
+	int	i;
+
+	i = 0;
+	while (i < size)
+	{
+		if (is_redir_node((*node_tab)[i]) == SUCCESS)
+		{
+			if ((i + 1) == size)
+			{
+				(*node_tab)[i].type = T_ERROR;
+				return ;
+			}
+			if ((*node_tab)[i + 1].type == T_HEREDOC || \
+				(*node_tab)[i + 1].type == T_REDIR_IN || \
+				(*node_tab)[i + 1].type == T_REDIR_OUT || \
+				(*node_tab)[i + 1].type == T_REDIR_OUT_APPEND || \
+				(*node_tab)[i + 1].type == T_PIPE)
+			{
+				(*node_tab)[i].type = T_ERROR;
+				return ;
+			}
+		}
+		i++;
+	}
+}
+
+void	check_pipe_node(t_node **node_tab, int size)
+{
+	const int	redir_id = get_node_id_pipe(node_tab, size);
+
+	if (redir_id >= 0)
+	{
+		if (redir_id - 1 < 0)
+		{
+			(*node_tab)[redir_id].type = T_ERROR;
+			return ;
+		}
+		if ((redir_id + 1) == size)
+		{
+			(*node_tab)[redir_id].type = T_ERROR;
+			return ;
+		}
+		if ((*node_tab)[redir_id + 1].type == T_PIPE)
+		{
+			(*node_tab)[redir_id].type = T_ERROR;
+			return ;
+		}
+	}
+}
---
+++ src/parser.c
@@ -0,0 +1,99 @@
+#include "../header/minishell.h"
+
+int	is_quote(char c)
+{
+	if (c == '\'' || c == '"')
+		return (SUCCESS);
+	return (FAIL);
+}
+
+char	*extract_str(t_node node)
+{
+	int		i;
+	int		start;
+	int		status;
+
+	i = 0;
+	start = 0;
+	status = OUT_QUOTE;
+	if (node.token[i] && (node.token[i] == '\'' || node.token[i] == '"'))
+	{
+		i++;
+		start = i;
+		status = IN_QUOTE;
+	}
+	while (node.token[i] && ((node.token[i] != ' ' && status == OUT_QUOTE) || \
+		((node.token[i] != '"' || node.token[i] != '\'') && \
+		status == IN_QUOTE)))
+	{
+		if ((node.token[i] == '"' || node.token[i] == '\'') && \
+			status == IN_QUOTE)
+			status = OUT_QUOTE;
+		i++;
+	}
+	if (node.token[i - 1] && is_quote(node.token[i - 1]) == SUCCESS)
+		i--;
+	return (ft_strdup(node.token, start, i));
+}
+
+char	*update_token(t_node **node_tab, int index)
+{
+	int		i;
+	int		j;
+	char	*new_token;
+	char	*tmp;
+
+	i = 0;
+	j = 0;
+	tmp = (*node_tab)[index + 1].token;
+	new_token = malloc(ft_strlen(tmp) - \
+				ft_strlen((*node_tab)[index].token) + 1);
+	if (!new_token)
+		return (0);
+	if (is_quote(tmp[j]) == SUCCESS)
+		j++;
+	while (tmp[j] && tmp[j++] == (*node_tab)[index].token[i])
+		i++;
+	while (tmp[j] && tmp[j] == ' ')
+		j++;
+	i = 0;
+	while (tmp[j])
+		new_token[i++] = tmp[j++];
+	new_token[i] = '\0';
+	return (free(tmp), new_token);
+}
+
+int	complete_redir_node(t_node **node_tab, int *size)
+{
+	int	i;
+
+	i = 0;
+	while (i < *size)
+	{
+		if (is_redir_node((*node_tab)[i]) == SUCCESS)
+		{
+			free((*node_tab)[i].token);
+			(*node_tab)[i].token = extract_str((*node_tab)[i + 1]);
+			if (!(*node_tab)[i].token)
+				return (FAIL);
+			(*node_tab)[i + 1].token = update_token(node_tab, i);
+			if (!(*node_tab)[i + 1].token)
+				return (FAIL);
+		}
+		i++;
+	}
+	return (SUCCESS);
+}
+
+int	parser(t_node **node_tab, int size, t_var_env **env_list, t_data *data)
+{
+	check_redir_node(node_tab, size);
+	check_pipe_node(node_tab, size);
+	if (check_error_node(node_tab, size, data) == FAIL)
+		return (FAIL);
+	if (check_env_var(node_tab, size, env_list, data) == FAIL)
+		return (FAIL);
+	if (complete_redir_node(node_tab, &size) == FAIL)
+		return (FAIL);
+	return (SUCCESS);
+}
